36c7e8dc70fdc06be69194b16181151bc4aeabd2
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2025-01-06 16:10:08 +0530

    hhh

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react'
 import{useParams} from 'react-router-dom'
-import{ShopContext} from '../context/shopContext'
+import{ShopContext} from '../context/ShopContext'
 import{FaStar, FaStarHalfStroke, FaTruckFast} from 'react-icons/fa6'
 import {FaHeart} from 'react-icons/fa'
 import {TbShoppingBagPlus} from 'react-icons/tb'
